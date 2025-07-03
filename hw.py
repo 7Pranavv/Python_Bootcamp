524375cd7ad72d0a565821cfd09b3d1d40f39faa
Pranav Rai <146828071+7Pranavv@users.noreply.github.com>
2025-07-03 20:40:06 +0530

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,3 @@
+#Write a Python program that prints a welcome message to the screen. Your task is to use the print() function to display the message:
+
+print("Hello World, I am ready to learn Python with Harry")
